245cca04503eb3185aadf0f58da6caeca7cba32a
JMFF8 <fuentesfernandezjosemanuel@gmail.com>
2024-03-05 19:24:32 +0100

    Delete Examen_Inicial

--- Examen_Inicial/app/src/main/java/iestr/gag/examen/model/Arma.kt
+++
@@ -1,8 +0,0 @@
-package iestr.gag.examen.model
-
-enum class Arma(val ataque:Float,val parada:Float,val esquive:Float) {
-    CUCHILLOS(0.5f,.25f,.75f),
-    ESPADA_ESCUDO(1f,0.5f,0f),
-    HACHA(0.75f,.25f,.5f),
-    MAZA(.75f,.5f,.25f)
-}
